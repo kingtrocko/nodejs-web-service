cc5cbfa16a1e499edce474e579abd1abf484ee44
Nahum Fabian <nahum_fabian13@hotmail.com>
2015-11-23 13:09:14 -0600

    initial commit with project folder structure

---
+++ server.js
@@ -0,0 +1,16 @@
+var express     = require('express');
+var app         = express();
+var bodyparser  = require('body-parser');
+
+var routes      = require('./routes');
+
+app.set('port', (process.env.PORT || 3000));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded( {entended: true}));
+
+app.use('/', routes);
+
+
+app.listen(app.get('port'), function(){
+  console.log('Node server is running on port', app.get('port'));
+});
